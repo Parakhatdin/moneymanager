711c97a1d9e0efd122300277337eaa0dc9e4e72c
Parakhatdin <nuratdinov.p@gmail.com>
2022-11-07 12:02:12 +0500

    psql connect

--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'moneymanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moneymanager',
     }
 }
 
