2b1855a7b7bfb26fe513df58bd3a5b3c1fe6615e
Parakhatdin <nuratdinov.p@gmail.com>
2022-11-13 22:26:57 +0500

    last changes

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mainapp.models import Category
+from mainapp.models import Envelope
+from mainapp.models import Family
+from mainapp.models import Member
+from mainapp.models import MemberAuthdata
+from mainapp.models import Transaction
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Envelope)
+admin.site.register(Family)
+admin.site.register(Member)
+admin.site.register(MemberAuthdata)
+admin.site.register(Transaction)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length = 150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "members"
+
+class MemberAuthdata(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length = 150)
+    member_id = models.ForeignKey(Member, on_delete = models.CASCADE)
+    class Meta:
+        db_table = "member_authdatas"
+
+class Family(models.Model):
+    name = models.CharField(max_length = 150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "families"
+
+class FamilyMember(models.Model):
+    member_id = models.BigIntegerField()
+    family_id = models.BigIntegerField()
+    class Meta:
+        db_table = "family_members"
+        unique_together = (('member_id', 'family_id'))
+
+class Envelope(models.Model):
+    name = models.CharField(max_length = 150)
+    member_id = models.ForeignKey(Member, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "envelopes"
+
+class Category(models.Model):
+    name = models.CharField(max_length = 150)
+    is_income = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    amount = models.BigIntegerField()
+    envelope_id = models.ForeignKey(Envelope, on_delete = models.CASCADE)
+    member_id = models.ForeignKey(Member, on_delete = models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return str(self.amount)
